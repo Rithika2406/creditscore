8e8970788d17ee240cbbdb39a30ee49d2ef4fa24
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2024-12-31 11:43:21 +0530

    Add files via upload

---
+++ creditscore.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  Scanner S=new Scanner(System.in);
+	  int x=S.nextInt();
+	  if(x>=750)
+	  {
+	      System.out.println("YES");
+	  }
+	  else{
+	      System.out.println("NO");
+	  }
+
+	}
+}
